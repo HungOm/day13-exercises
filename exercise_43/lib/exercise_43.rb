c3246bed549982cdd1ad5f98549c321027534a3a
Om <om@outlook.com>
2019-06-03 13:17:51 +0800

    exercise 43 changes update

--- exercise_43/lib/exercise_43.rb
+++ exercise_43/lib/exercise_43.rb
@@ -1,1 +1,55 @@
 
+def average(a,b)
+    a =a.to_f
+    b =b.to_f
+    (a+b)/2
+
+end
+
+# ==========================================
+def average_array(a)
+    a.inject{|ac,elements|ac+elements}.to_f/a.size
+end
+
+def repeat(a,b)
+
+    a * b
+
+end
+# ===========================================
+
+def yell(a)
+    a.upcase + "!"
+end
+#===========================================
+def alternating_case(a)
+    list = a.downcase.split(" ")
+
+    # puts "#{list}"
+    
+    # list of words broke down from sentence by split methods
+    new_list =[]
+    
+    list.each_with_index do |word, index|
+        if index.even?
+            new_list << word.upcase
+        else
+            new_list << word
+        end
+    end
+
+    # new_list.join(" ")
+    return new_list.join(" ")
+
+
+end
+# ===============================================
+
+def hipsterfy(word)
+    #get the last vowels of a word using regex match
+    
+    new_word = word.sub(/[aeiou](?=[^aeiou]*\z)/,"")
+    # puts new_word
+end
+
+# hipsterfy("Hey I am good")
